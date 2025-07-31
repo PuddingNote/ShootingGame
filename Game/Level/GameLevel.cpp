50a8e925ee5f718d90082157789f5dc385bb0663
Pudding <122201777+PuddingNote@users.noreply.github.com>
2025-07-31 16:03:08 +0900

    feat: 충돌 판정 기능 추가 & 점수 처리 및 게임 종료 판정(게임 데모 완성)
    - 간단한 AABB 방식으로 충돌 판정 기능 구현 (Actor)
    - GameLevel에서 충돌 처리
    - 점수 처리 및 게임 종료 판정 (GameLevel)
    - 플레이어가 죽으면 게임 종료

--- Game/Level/GameLevel.cpp
+++ Game/Level/GameLevel.cpp
@@ -1,7 +1,12 @@
 #include "GameLevel.h"
 #include "Actor/Player.h"
 #include "Actor/Enemy.h"
+#include "Actor/PlayerBullet.h"
+#include "Actor/EnemyBullet.h"
+#include "Engine.h"
 #include "Utils/Utils.h"
+
+#include <iostream>
 
 // 적 생성할 때 사용할 글자 값 (여기서 static은 private임)
 static const char* enemyType[] =
@@ -39,6 +44,18 @@ void GameLevel::Tick(float deltaTime)
 	super::Tick(deltaTime);
 
 	// 적 생성
+	SpawnEnemies(deltaTime);
+
+	// 플레이어 탄약과 적의 충돌 처리
+	ProcessCollisionPlayerBulletAndEnemy();
+
+	// 적의 탄약과 플레이어의 충돌 처리
+	ProcessCollisionPlayerAndEnemyBullet();
+}
+
+void GameLevel::SpawnEnemies(float deltaTime)
+{
+	// 적 생성
 	enemySpawnTimer.Tick(deltaTime);
 
 	// 타임 아웃 확인
@@ -61,7 +78,128 @@ void GameLevel::Tick(float deltaTime)
 	AddActor(new Enemy(enemyType[index], yPosition));
 }
 
+void GameLevel::ProcessCollisionPlayerBulletAndEnemy()
+{
+	std::vector<PlayerBullet*> bullets;
+	std::vector<Enemy*> enemies;
+
+	// 두 타입의 액터를 검색해서 배열 채우기
+	for (Actor* const actor : actors)
+	{
+		PlayerBullet* bullet = actor->As<PlayerBullet>();
+		if (bullet)
+		{
+			bullets.emplace_back(bullet);
+			continue;
+		}
+
+		Enemy* enemy = actor->As<Enemy>();
+		if (enemy)
+		{
+			enemies.emplace_back(enemy);
+		}
+	}
+
+	// 예외처리
+	if (bullets.size() == 0 || enemies.size() == 0)
+	{
+		return;
+	}
+
+	// 충돌 처리
+	for (auto* bullet : bullets)
+	{
+		for (auto* enemy : enemies)
+		{
+			// 두 액터가 서로 겹쳤는지 확인
+			if (bullet->TestIntersect(enemy))
+			{
+				enemy->Destroy();
+				bullet->Destroy();
+
+				// 점수 증가 처리
+				score++;
+
+				continue;
+			}
+		}
+	}
+}
+
+void GameLevel::ProcessCollisionPlayerAndEnemyBullet()
+{
+	Player* player = nullptr;
+	std::vector<EnemyBullet*> bullets;
+
+	for (Actor* const actor : actors)
+	{
+		EnemyBullet* bullet = actor->As<EnemyBullet>();
+
+		if (bullet)
+		{
+			bullets.emplace_back(bullet);
+			continue;
+		}
+
+		// 플레이어 확인
+		if (!player)
+		{
+			player = actor->As<Player>();
+		}
+	}
+
+	// 안해도 되는 상황 확인
+	if (bullets.size() == 0 || !player)
+	{
+		return;
+	}
+
+	// 충돌 확인
+	for (auto* bullet : bullets)
+	{
+		if (player->TestIntersect(bullet))
+		{
+			// 게임 종료 처리
+			isPlayerDead = true;
+			playerDeadPosition.x = player->Position().x + player->Width() / 2;
+			playerDeadPosition.y = player->Position().y;
+
+			player->Destroy();
+			bullet->Destroy();
+			break;
+		}
+	}
+}
+
 void GameLevel::Render()
 {
 	super::Render();
+
+	if (isPlayerDead)
+	{
+		Utils::SetConsoleTextColor(Color::Red);
+		Utils::SetConsolePosition(Vector2(playerDeadPosition.x, playerDeadPosition.y - 2));
+		std::cout << "   .   ";
+
+		Utils::SetConsolePosition(Vector2(playerDeadPosition.x, playerDeadPosition.y - 1));
+		std::cout << " .  .  .";
+
+		Utils::SetConsolePosition(Vector2(playerDeadPosition.x, playerDeadPosition.y - 0));
+		std::cout << "..:V:..";
+
+		Utils::SetConsolePosition(Vector2(playerDeadPosition.x, playerDeadPosition.y + 1));
+		std::cout << "Game Over!";
+
+		Sleep(2000);
+		Engine::Get().Quit();
+	}
+
+	// 스코어 보여주기
+	char buffer[20] = {};
+	sprintf_s(buffer, 20, "Score: %d", score);
+
+	// 출력
+	Utils::SetConsolePosition(Vector2(1, Engine::Get().Height() + 2));
+	Utils::SetConsoleTextColor(Color::White);
+	std::cout << buffer;
 }
--- Game/Level/GameLevel.h
+++ Game/Level/GameLevel.h
@@ -2,10 +2,9 @@
 
 #include "Level/Level.h"
 #include "Utils/Timer.h"
-
+#include "Math/Vector2.h"
 /*
 * Todo: 게임 레벨의 책임
-* - 적 생성 : 약간의 랜덤성을 가미해서 일정 시간마다 적 생성
 * - 충돌 처리 : 플레이어-적 탄약 사이의 충돌 / 적-플레이어 탄약의 충돌 처리(AABB)
 * - 점수 관리 : 플레이어가 적을 없애면 1점씩 획득
 * - 게임 판정 : 플레이어가 죽으면 게임 종료
@@ -23,8 +22,22 @@ public:
 	virtual void BeginPlay() override;
 	virtual void Tick(float deltaTime) override;
 	virtual void Render() override;
+	
+private:
+	void SpawnEnemies(float deltaTime);
+	void ProcessCollisionPlayerBulletAndEnemy();
+	void ProcessCollisionPlayerAndEnemyBullet();
 
 private:
 	// 적 생성시 시간 계산을 위한 타이머
 	Timer enemySpawnTimer;
+
+	// 점수 변수
+	int score = 0;
+
+	// 플레이어의 죽음 처리를 위한 변수
+	bool isPlayerDead = false;
+
+	// 플레이어 죽은 위치
+	Vector2 playerDeadPosition;
 };
